e74915d9fe0b3fb98e757a4cda5787ec6d6063c4
Hadas <hadasadam8@gmail.com>
2022-05-08 20:46:12 +0300

    fix onClick login bug & change token

--- app/src/main/java/com/example/musimatch/LinkPostFragment.java
+++ app/src/main/java/com/example/musimatch/LinkPostFragment.java
@@ -1,41 +1,22 @@
 package com.example.musimatch;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.musimatch.adapters.PostAdapter;
 import com.example.musimatch.models.Post;
 import com.example.musimatch.models.PostModel;
-import com.example.musimatch.services.LoginService;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+
 
 import java.util.ArrayList;
 
 public class LinkPostFragment extends Fragment {
 
     private static final String TAG = "LinkPostFragment";
-
-    private MenuItem signOutButton;
-    private MenuItem signInButton;
-    private MenuItem loggedInUsername;
 
     private View view;
     private ArrayList<Post> postsArrayList;
@@ -77,89 +58,5 @@ public class LinkPostFragment extends Fragment {
             myPost.getLinkedPostsIds().add(destinationPost.getId());
             destinationPost.getLinkedPostsIds().add(myPost.getId());
         }
-    }
-
-    /************************************ Login Methods ************************************/
-
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.user_top_menu, menu);
-        loggedInUsername = menu.findItem(R.id.logged_in_display_name);
-        signInButton = menu.findItem(R.id.google_sign_in_button);
-        signOutButton = menu.findItem(R.id.google_sign_out_button);
-
-        if (LoginService.getInstance(this.getContext()).getGoogleAccount() != null && !LoginService.getInstance(this.getContext()).isLoggedIn()) {
-            LoginService.getInstance(this.getContext()).signOut();
-        }
-
-        setSignedInView(LoginService.getInstance(this.getContext()).getUserDisplayName(),
-                LoginService.getInstance(this.getContext()).isLoggedIn());
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.google_sign_in_button:
-                signIn();
-                return true;
-            case R.id.google_sign_out_button:
-                signOut();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == LoginService.RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                LoginService.getInstance(this.getContext()).setGoogleAccount(account);
-                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getDisplayName());
-                Task<AuthResult> authResultTask = LoginService.getInstance(this.getContext()).firebaseAuthWithGoogle(account.getIdToken());
-
-                authResultTask.addOnCompleteListener(this.getActivity(), firebaseLoginRes -> {
-                    if (firebaseLoginRes.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithCredential:success");
-                        setSignedInView(LoginService.getInstance(this.getContext()).getGoogleAccount().getDisplayName(), true);
-                        LoginService.getInstance(this.getContext()).createNewUser(authResultTask);
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithCredential:failure", firebaseLoginRes.getException());
-                    }
-                });
-
-                setSignedInView(account.getDisplayName(), true);
-            } catch (ApiException e) {
-                Log.w(TAG, "Google sign in failed", e);
-                e.printStackTrace();
-            } catch (NullPointerException e) {
-                Log.w(TAG, "Null pointer exception while login trial", e);
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void setSignedInView(String displayName, boolean isLoggedIn) {
-        this.loggedInUsername.setTitle(displayName);
-        this.signInButton.setVisible(!isLoggedIn);
-        this.signOutButton.setVisible(isLoggedIn);
-    }
-
-    private void signIn() {
-        Log.d(TAG,"Trying to sign in...");
-        Intent signInIntent = LoginService.getInstance(this.getContext()).getGoogleSignInClient().getSignInIntent();
-        startActivityForResult(signInIntent, LoginService.RC_SIGN_IN);
-    }
-
-    private void signOut() {
-        Log.d(TAG,"Trying to sign out...");
-        LoginService.getInstance(this.getContext()).signOut();
-        setSignedInView(this.getString(R.string.default_sign_in_name_display), false);
     }
 }
