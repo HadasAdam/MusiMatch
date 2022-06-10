cbbd4197ab82dff7e47a35d16a3cea92acbbc150
Hadas <hadasadam8@gmail.com>
2022-06-10 13:39:17 +0300

    fix script

--- app/src/main/java/com/example/musimatch/db/linking_tables/POSTS_TO_POSTS.sql
+++ app/src/main/java/com/example/musimatch/db/linking_tables/POSTS_TO_POSTS.sql
@@ -1,11 +1,11 @@
 create table POSTS_TO_POSTS
 (
-    SOURCE_POST             NUMBER not null,
-    LINKED_POST             NUMBER not null
+    SOURCE_POST_ID             NUMBER not null,
+    LINKED_POST_ID             NUMBER not null
 )
 
-alter table POSTS_TO_POSTS add constraint POSTS_TO_POSTS_UK unique (SOURCE_POST,LINKED_POST);
-alter table POSTS_TO_POSTS add constraint SOURCE_POST_FK foreign key (POST_ID)
+alter table POSTS_TO_POSTS add constraint POSTS_TO_POSTS_UK unique (SOURCE_POST_ID,LINKED_POST_ID);
+alter table POSTS_TO_POSTS add constraint SOURCE_POST_FK foreign key (SOURCE_POST_ID)
     references POSTS(ID);
-alter table POSTS_TO_POSTS add constraint LINKED_POST_FK foreign key (POST_ID)
+alter table POSTS_TO_POSTS add constraint LINKED_POST_FK foreign key (LINKED_POST_ID)
     references POSTS(ID);
