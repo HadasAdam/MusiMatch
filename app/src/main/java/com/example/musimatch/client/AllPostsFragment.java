f5e1577239b1de6f9d3a12e9525b803ed4409533
Hadas <hadasadam8@gmail.com>
2022-06-14 00:52:31 +0300

    add option to always go back to my profile

--- app/src/main/java/com/example/musimatch/client/AllPostsFragment.java
+++ app/src/main/java/com/example/musimatch/client/AllPostsFragment.java
@@ -10,15 +10,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.musimatch.R;
 import com.example.musimatch.client.adapters.PostAdapter;
 import com.example.musimatch.models.Post;
 import com.example.musimatch.models.PostModel;
+import com.example.musimatch.services.LoginService;
+
 import java.util.ArrayList;
 
 
 public class AllPostsFragment extends Fragment {
+
+    Button profileButton;
 
     public AllPostsFragment(){}
 
@@ -37,6 +42,15 @@ public class AllPostsFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_all_posts, container, false);
         postsArrayList = new ArrayList<Post>(PostModel.instance.getAllPosts());
+        profileButton = view.findViewById(R.id.allPosts_profileBtn);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AllPostsFragmentDirections.ActionAllPostsFragmentToUserProfileFragment4 action =
+                        AllPostsFragmentDirections.actionAllPostsFragmentToUserProfileFragment4(LoginService.getUser());
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
         RecyclerView recyclerView = view.findViewById(R.id.allPostsFRG_recyclerView);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         recyclerView.hasFixedSize();
